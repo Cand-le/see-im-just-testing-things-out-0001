9074c0911dd3fb6923ba1b62c2a99e8aba33f4f9
Cand-le <88761172+Cand-le@users.noreply.github.com>
2025-12-24 19:40:38 +0000

    Update main.c
    the comment was wrong!!!

--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(void){
-    int w_imminentDeath = 0; # Set to 999 if you want the program to return an error!
+    int w_imminentDeath = 0; // Set to 999 if you want the program to return an error!
     if(w_imminentDeath == 999){
         return 1;
     }

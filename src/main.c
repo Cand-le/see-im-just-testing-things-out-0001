607bb6bacb97689eae86271ebf7860a388dbada1
Cand-le <88761172+Cand-le@users.noreply.github.com>
2025-12-24 19:51:48 +0000

    Update main.c
    removed redundancy

--- src/main.c
+++ src/main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main(void){
-    int w_imminentDeath = 0; // Set to 999 if you want the program to return an error!
-    if(w_imminentDeath == 999){
+    int w_imminentDeath = 0; // Set to 99 if you want the program to return an error!
+    if(w_imminentDeath == 99){
         return 1;
     }
     return 0;
